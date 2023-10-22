c05c63c31cb5700fa48866e55eb620c26fb003c0
francofil <francofil03@gmail.com>
2023-10-22 13:28:18 -0300

    Parte 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,21 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            IFilter neg = new FilterNegative();
+            IFilter grey = new FilterGreyscale();
+
+            IPipe pipenull = new PipeNull();
+            IPipe pipe2 = new PipeSerial(neg, pipenull);
+            IPipe pipe1 = new PipeSerial(grey, pipe2);
+
+            pipe1.Send(picture);
+            IPicture image = pipe2.Send(picture);
+
+            provider.SavePicture(image, @"beer2.jpg");
+
         }
     }
 }
